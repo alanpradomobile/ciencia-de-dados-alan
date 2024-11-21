af31f810ad2008f2acf2a5a3e91de1ef20aa8ed0
alan prado <107581951+alanpradomobile@users.noreply.github.com>
2024-11-21 16:44:07 -0300

    21/11

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,1 +1,8 @@
 const url = 'https://raw.githubusercontent.com/alanpradomobile/ciencia-de-dados-alan/refs/heads/main/dados-globais.json';
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url);
+
+    const dados = await res.json();
+    
+}
